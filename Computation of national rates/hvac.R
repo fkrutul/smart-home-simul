1c755b815542a753489b2758ea2306de8002c21e
fkrutul <61340559+fkrutul@users.noreply.github.com>
2023-02-17 18:00:45 -0600

    Delete Computation of national rates directory

--- Computation of national rates/hvac.R
+++
@@ -1,7 +0,0 @@
-getwd()
-setwd(getwd())
-electric = readxl::read_xls("table6.xls")
-mean(electric$Price)
-weightedmean = mean(rep(electric$Price,electric$Customers))
-toDollars = weightedmean/100
-print(toDollars)
